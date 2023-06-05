479628b6412ca16c9179eec78edf0b151767c04f
aditi <40533629+aditixyz@users.noreply.github.com>
2023-06-05 13:13:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
-var submitURL = 'https://api.airtable.com/v0/appPJOzBmRaGNwHHM/Table%201?api_key=key0uDPUEu13V831Z';
-var form = $('#my-form');
-form.on('submit', function(e){
-   e.preventDefault();
-   var email = $(this).find('input[name=email]').val();
+$(document).ready(function(){
+    var submitURL = 'https://api.airtable.com/v0/appPJOzBmRaGNwHHM/Table%201?api_key=key0uDPUEu13V831Z';
+    var form = $('#my-form');
+    form.on('submit', function(e){
+       e.preventDefault();
+       var email = $(this).find('input[name=email]').val();
 
-   if (!email) {
-      $(this).find('input[name=email]').addClass("error");
-     return;
-   }
-   var data = {
-     'fields': {
-       'Email': email,
-    }
-   };
-  $.post(submitURL, data, function(data){
-     $('#submit-message').text('Submitted!!!!');
-     console.log('success',data)
-  });
+       if (!email) {
+          $(this).find('input[name=email]').addClass("error");
+         return;
+       }
+       var data = {
+         'fields': {
+           'Email': email,
+        }
+       };
+      $.post(submitURL, data, function(data){
+         $('#submit-message').text('Submitted!!!!');
+         console.log('success',data)
+      });
+    });
 });
