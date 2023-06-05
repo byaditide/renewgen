c6a8eea2dc21e056d6863c90cbbb8aedec9b56d6
aditi <40533629+aditixyz@users.noreply.github.com>
2023-06-05 13:16:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var submitURL = 'https://api.airtable.com/v0/appPJOzBmRaGNwHHM/Table%201?api_key=key0uDPUEu13V831Z';
+    var submitURL = 'https://api.airtable.com/v0/appPJOzBmRaGNwHHM/Table%201';
     var form = $('#my-form');
     form.on('submit', function(e){
        e.preventDefault();
@@ -14,9 +14,22 @@ $(document).ready(function(){
            'Email': email,
         }
        };
-      $.post(submitURL, data, function(data){
-         $('#submit-message').text('Submitted!!!!');
-         console.log('success',data)
-      });
+       $.ajax({
+           url: submitURL,
+           method: "POST",
+           headers: {
+               'Authorization': 'Bearer key0uDPUEu13V831Z',
+               'Content-Type': 'application/json'
+           },
+           data: JSON.stringify(data),
+           success: function(data){
+                $('#submit-message').text('Submitted!!!!');
+                console.log('success',data)
+           },
+           error: function(error){
+                $('#submit-message').text('Error occurred!');
+                console.log('error',error)
+           }
+       });
     });
 });
